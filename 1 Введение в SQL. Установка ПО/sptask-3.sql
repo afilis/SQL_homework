11858fb9e21a41cb7bbf8b5e67a7c064e89e063c
Ilia Afremov <afremov_i@mail.ru>
2021-05-21 00:18:05 +0400

    - правка ошибок, удалены лишние файлы

--- 1 Введение в SQL. Установка ПО/sptask-3.sql
+++ 1 Введение в SQL. Установка ПО/sptask-3.sql
@@ -1,14 +1,16 @@
 select kcu.table_name,
-	tco.constraint_name,
+    tco.constraint_name,
     kcu.column_name,
     cols.data_type
 from information_schema.table_constraints tco
-join information_schema.key_column_usage kcu 
+join information_schema.key_column_usage kcu
     on kcu.constraint_name = tco.constraint_name
-    and kcu.table_name = tco.table_name 
+    and kcu.constraint_schema = tco.constraint_schema
+    and kcu.table_name = tco.table_name
 join information_schema.columns cols
     on cols.column_name = kcu.column_name
-    and cols.table_name = kcu.table_name 
+    and cols.table_name = kcu.table_name
+    and cols.table_schema = kcu.constraint_schema
 where tco.table_schema = 'public'
-	and tco.constraint_type = 'PRIMARY KEY'
+    and tco.constraint_type = 'PRIMARY KEY'
 order by table_name, constraint_name, column_name;
